5edb9e05e6b7cfeb17b323a714e4409a434cb3fb
kimthornton <kimthornton@enginsol.com>
2016-08-02 15:30:56 -0400

    Added MOTD Info

--- manifests/site.pp
+++ manifests/site.pp
@@ -44,4 +44,12 @@ node default {
   #   class { 'my_class': }
   notify { "Hello, my name is ${::hostname}": }
   notify { "Hello, Kim": }
+  
+  file {'/etc/motd":
+    ensure => file,
+    owner => 'root',
+    group => 'root',
+    mode => '0644',
+    content => "Today I learned what it means to manage state using Puppet.\n",
+    }
 }
