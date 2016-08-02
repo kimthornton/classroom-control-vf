c6cb5f1b7f30560974877785ee78f6ecc6d7c004
kimthornton <kimthornton@enginsol.com>
2016-08-02 16:06:02 -0400

    Added the update to motd using cowsay;

--- manifests/site.pp
+++ manifests/site.pp
@@ -50,11 +50,8 @@ node default {
     provider => gem,
     }
     
-  file {'/etc/motd':
-    ensure => file,
-    owner => 'root',
-    group => 'root',
-    mode => '0644',
-    content => "Today is a good day for puppet.\n",
+  exec {cowsay 'Welcome to ${::fqdn}!' > /etc/motd":
+    path => '/usr:/usr/bin:/usr/local/bin',
+    creates => '/etc/motd',
     }
 }
