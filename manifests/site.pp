57e4ac2eb6f59a73b569a2c04341ad33b62d41db
kimthornton <kimthornton@enginsol.com>
2016-08-03 08:24:26 -0400

    Added Testing of host resource

--- manifests/site.pp
+++ manifests/site.pp
@@ -62,4 +62,9 @@ node default {
     path => '/usr:/usr/bin:/usr/local/bin',
     creates => '/etc/motd',
     }
+    
+  host { 'testing.puppetlabs.vm':
+    ensure => present,
+    ip => '127.0.0.1',
+    }
 }
