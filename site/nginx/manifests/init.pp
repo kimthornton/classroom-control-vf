70ceb61d2363e61d07bd44978fd8d030ccb3ea2b
kimthornton <kimthornton@enginsol.com>
2016-08-04 15:11:13 -0400

    Update init.pp

--- site/nginx/manifests/init.pp
+++ site/nginx/manifests/init.pp
@@ -1,42 +1,63 @@
 class nginx {
-  package { 'nginx':
-    ensure => present,
+  case $::osfamily {
+  'redhat','debian':{
+    $package = 'nginx'
+    $owner = 'root'
+    $group = 'root'
+    $docroot = '/var/www'
+    $confdir - '/etc/nginx'
+    $logdir = '/var/log/nginx'
   }
+  'windows':{
+    $package = 'nginx-service'
+    $owner = 'Administrator'
+    $group = 'Administrator'
+    $docroot = 'C:/ProgramData/nginx/html'
+    $confdir = 'C:/ProgramData/nginx'
+    $logdir = 'C:/ProgramData/nginx/logs;
+  }
+  default :{
+    fail("Module ${module_name} is not supported on ${::osfamily}")
+  }
+  
+  $user = $::osfamily ? {
+    'redhat' => 'nginx',
+    'debian' => 'www-data',
+    'windows' => 'nobody',
+  }
+  
   
   File {
     owner => 'root',
     group => 'root',
     }
+  
+    package {$package:
+      ensure => present,
+    }
     
-  file { '/var/www':
+  file { [$docroot, "${confdir/conf.d"]:
     ensure => directory,
     mode => '0775'
   }
   
-  file { '/var/www/index.html':
+  file { "${docroot}/index.html":
     ensure => file,
     mode => '0664',
     source => 'puppet:///modules/nginx/index.html',
   }
   
-  file {'/etc/nginx/nginx.conf':
+  file {"${confdir}/nginx.conf":
    ensure => file,
     mode => '0664',
-    source => 'puppet:///modules/nginx/nginx.conf',
-    require => Package['nginx'],
+    content => template('nginx/nginx.conf.erb'),
     notify => Service['nginx'],
   }
   
-  file {'/etc/nginx/conf.d':
-    ensure => directory,
-      mode => '0775'
-  }
-  
-  file {'/etc/nginx/conf.d/default.conf':
-   ensure => file,
+  file {"${confdir}/conf.d/default.conf":
+    ensure => file,
     mode => '0664',
-    source => 'puppet:///modules/nginx/default.conf',
-    require => Package['nginx'],
+    content => template('nginx/default.conf.erb'),
     notify => Service['nginx'],
   }
   
