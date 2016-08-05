ae7a2beec8884a00204846f2deb5161c0298af6d
kimthornton <kimthornton@enginsol.com>
2016-08-05 10:56:17 -0400

    Update params.pp

--- site/nginx/manifests/params.pp
+++ site/nginx/manifests/params.pp
@@ -1,1 +1,28 @@
 class nginx::params {
+  case $::os['family'] {
+  'redhat','debian':{
+    $package  = 'nginx'
+    $owner    = 'root'
+    $group    = 'root'
+    $docroot  = '/var/www'
+    $confdir  = '/etc/nginx'
+    $logdir   = '/var/log/nginx'
+  }
+  'windows' : {
+    $package  = 'nginx-service'
+    $owner    = 'Administrator'
+    $group    = 'Administrators'
+    $docroot  = 'C:/ProgramData/nginx/html'
+    $confdir  = 'C:/ProgramData/nginx'
+    $logdir   = 'C:/ProgramData/nginx/logs'
+  }
+  default : {
+    fail("Module ${module_name} is not supported on ${::os['family']}")
+  }
+ }
+ $user = $::os['family'] ? {
+  'redhat'  => 'nginx',
+  'debian'  =>  'www-data',
+  'windows  => 'nobody',
+  }
+}
